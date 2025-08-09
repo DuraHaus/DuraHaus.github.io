334007c4c89cf23f884e488e66f79f07f3ca3b58
DuraHaus <durahaus.solutions@gmail.com>
2025-08-08 20:27:04 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+// Minimal JS: mobile menu and smooth anchors
+document.addEventListener('click', (e)=>{
+  const a = e.target.closest('a[href^="#"]');
+  if(a){
+    const el = document.querySelector(a.getAttribute('href'));
+    if(el){ e.preventDefault(); el.scrollIntoView({behavior:'smooth'}); }
+  }
+});
